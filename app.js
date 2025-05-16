70762aca64f9245c2c837fbc65101a21a465911e
Greythecoder <megauravsingh28@gmail.com>
2025-05-17 02:45:12 +0530

    code the second verse

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
 let gameseq = [];
 let userseq = [];
 
-let started = false ;
+let btns = ["yellow", "red", "purple", "green"];
+
+let started = false;
 let level = 0;
 
-document.addEventListener( "keypress " , function (){ 
-    if (game === false){
+let h2 = document.querySelector("h2");
+
+document.addEventListener("keypress", function () {
+    if (!started) {
         console.log("game started");
         started = true;
-        
+        levelup();
     }
+});
 
-})
+function btnflash(btn) {
+    btn.classList.add("flash");
+    setTimeout(function () {
+        btn.classList.remove("flash");
+    }, 250);
+}
+
+function levelup() {
+    level++;
+    h2.innerText = `Level ${level}`;
+
+    let randidx = Math.floor(Math.random() * 4); 
+    let randcolor = btns[randidx];
+    let randbtn = document.querySelector(`.${randcolor}`);
+
+    gameseq.push(randcolor);
+
+    console.log(randidx);
+    console.log(randcolor);
+    console.log(randbtn);
+    
+    btnflash(randbtn);
+}
