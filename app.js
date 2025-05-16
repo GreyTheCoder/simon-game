8dfc7c50e30207bdee08f2e692f45b4415bac363
Greythecoder <megauravsingh28@gmail.com>
2025-05-16 17:34:28 +0530

    updated js

---
+++ app.js
@@ -0,0 +1,14 @@
+let gameseq = [];
+let userseq = [];
+
+let started = false ;
+let level = 0;
+
+document.addEventListener( "keypress " , function (){ 
+    if (game === false){
+        console.log("game started");
+        started = true;
+        
+    }
+
+})
